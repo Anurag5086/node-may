fcb68f27b64bf24c0b0e7098fe50688c6daa55fc
Anurag5086 <anuraggupta5086@gmail.com>
2026-05-13 22:24:47 +0530

    blog done

--- blog-api/controllers/blogController.js
+++ blog-api/controllers/blogController.js
@@ -1,39 +1,86 @@
-const blogData = require('../models/blogData')
+const Blog = require("../models/Blog")
 
-exports.getAllBlogs = (req, res) => {
-    res.status(200).json({success: true, blogData})
+exports.getAllBlogs = async (req, res) => {
+    try{
+        const blogs = await Blog.find()
+        res.status(200).json({success: true, blogs})
+    }catch(err){
+        res.status(500).json({success: false, message: "Internal Server Error!"})
+    }
 }
 
-exports.getBlogById = (req, res) => {
-    const blogId = parseInt(req.params.id)
+exports.getBlogById = async (req, res) => {
+    try{
+        const blogId = parseInt(req.params.id)
 
-    const blog = blogData.find(blog => blog.id === blogId)
+        // const blog = await Blog.find({_id: blogId}) -----> [{}]
+        // const blog = await Blog.findOne({_id: blogId}) ----> {}
+        const blog = await Blog.findById(blogId)
+        if(!blog){
+            res.status(404).json({success: false, message: "Blog not found"})
+        }
 
-    if(blog){
         res.status(200).json({success: true, blog})
-    }else{
-        res.status(404).json({success: false, message: "Blog not found"})
+
+    }catch(err){
+        res.status(500).json({success: false, message: "Internal Server Error!"})
     }
 }
 
-exports.createBlog = (req, res) => {
-    const { id, title, description } = req.body
-    
-    if(!id || !title || !description){
-        res.status(400).json({success: false, message: "All fields required!"})
+exports.createBlog = async (req, res) => {
+    try{
+        const { title, description } = req.body
+        
+        if(!title || !description){
+            res.status(400).json({success: false, message: "All fields required!"})
+        }
+
+        // const blog = new Blog(
+        //     title,
+        //     description
+        // )
+
+        // await blog.save()
+
+        const blog = await Blog.create(req.body)
+        res.status(201).json({success: true, message: "Blog created successfully!"})
+    }catch(err){
+        res.status(500).json({success: false, message: "Internal Server Error!"})
     }
 
-    const newBlog = {
-        id,
-        title,
-        description
+}
+
+exports.updateBlog = async (req, res) => {
+    try{
+        const blogId = parseInt(req.params.id)
+
+        const blog = await Blog.findById(blogId)
+        if(!blog){
+            res.status(404).json({success: false, message: "Blog not found!"})
+        }
+
+        const updatedBlog = await Blog.findByIdAndUpdate(blogId, req.body)
+
+        res.status(200).json({success: true, message: "Blog updated successfully!", updatedBlog})
+    }catch(err){
+        res.status(500).json({success: false, message: "Internal Server Error!"})
     }
+}
 
-    console.log(blogData)
 
-    blogData.push(newBlog)
+exports.deleteBlog = async (req, res) => {
+    try{
+        const blogId = parseInt(req.params.id)
 
-    console.log(blogData)
+        const blog = await Blog.findById(blogId)
+        if(!blog){
+            res.status(404).json({success: false, message: "Blog not found!"})
+        }
 
-    res.status(201).json({success: true, message: "Blog created successfully!"})
+        await Blog.findByIdAndDelete(blogId)
+
+        res.status(200).json({success: true, message: "Blog deleted successfully!"})
+    }catch(err){
+        res.status(500).json({success: false, message: "Internal Server Error!"})
+    }
 }
